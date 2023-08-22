9d60a4c6f548c2438487bbca6975f998482d9742
Adarsha-React <adarshareact2023@gmail.com>
2023-08-22 12:36:29 +0530

    Basic layout created for todo list app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import "./App.css";
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="app">
+      <div className="container">
+        <h3>Todo-list Application</h3>
+        <form className="todoForm">
+          <input type="text" />
+          <button>Add</button>
+        </form>
+        <ul className="todoList">
+          <li className="singleTodo">
+            <span className="todoName">Learn React</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default App;
